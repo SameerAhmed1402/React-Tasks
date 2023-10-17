d34c21222f2824823d2a20793c6273103c8af549
SameerAhmed1402 <sameerkuraishi1402@gmail.com>
2023-10-17 20:11:40 +0530

    using hook task

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,30 @@
-// State: state refers to the data
-// that is used to manage and represent the internal state of the component.
-// it allows the components to keep track of and manage their own data -> the data will change over the time as a result of user interactions, data fetching, or other factors.
+import React, { useState } from 'react';
 
-// whenever the state of a component changes, it will cause re-rendering of the component
+function App() {
 
-import React, { Component } from 'react';
+  const [counter, setCounter] = useState(0);
 
-class App extends Component {
 
-  constructor(props) {
-    super(props); // to call the parent class constructor
-
-    // define the state
-    this.state = {
-      count: 0,
-    }
+  function handleClick() {
+    setCounter(counter + 1);
+  }
+  function handling(){
+    setCounter(counter-1);
+  }
+  function refresh(){
+    setCounter(0);
   }
 
-  increment = () => {
-    // update the state (data) of the count
-    this.setState({
-      // provide the new state
-      count: this.state.count + 1,
-    })
-  }
-  decrement=()=>{
-    this.setState({
-      count:this.state.count-1,
-    })
-  }
-  refresh=()=>{
-    this.setState({
-    count:0,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <p>Count: { this.state.count }</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        <button onClick={this.refresh}>Refresh</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <p>{ counter }</p>
+      <button onClick={handleClick}>plus</button>
+      <button onClick={handling}>minus</button>
+      <button onClick={refresh}>restart</button>
+    </div>
+  )
 }
 
 export default App;
-
 
 
